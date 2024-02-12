56c478fdc8de49222753fffd46fa883fafc22557
caiosilvaferreira <caiosilvaferreira1@outlook.com.br>
2024-02-12 16:35:07 -0300

    foi realizado autenticação, autorização, otimização de api. Trabalhei na compressão de dados, memoria cache para otimizar o serviço web, api restfull voltando os dados em json no postman

--- Program.cs
+++ Program.cs
@@ -56,6 +56,7 @@ void ConfigureAuthentication(WebApplicationBuilder builder)
 void ConfigureMvc(WebApplicationBuilder builder)
 {
     builder.Services.AddMemoryCache();
+
     builder.Services.AddResponseCompression(options =>
     {
         // options.Providers.Add<BrotliCompressionProvider>();
